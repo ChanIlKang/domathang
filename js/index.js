b0b3258cbbcd6dd4cd3fb0d3f2ae6adeb21d08a8
KangChanil <cikang@eqqui.com>
2025-01-09 10:30:24 +0900

    feat: create About Us modal

--- js/index.js
+++ js/index.js
@@ -27,6 +27,29 @@ menu.forEach((menuItem, index) => {
     })
 })
 
+const aboutUs = document.querySelector('#aboutUs');
+const isAboutUsOn = false;
+
+document.getElementById('aboutUsLink').addEventListener('click', showAboutUs);
+
+function showAboutUs() {
+    if (!isAboutUsOn) {
+        aboutUs.style.display = 'block';
+    }
+
+    for(let i=0; i<menu.length; i++){
+        console.log("1")
+        menu[i].classList.remove('active');
+        console.log("2")
+    }
+
+    for(let i=0; i<subMenu.length; i++){
+        console.log("3")
+        subMenu[i].classList.remove('active');
+        console.log("4")
+    }
+}
+
 // addEventListener('DOMContentLoaded', () => {
 //
 // })
