24d2adeb5eb586f68a825061d9d24cdf9289956c
KangChanil <cikang@eqqui.com>
2025-03-18 06:05:35 +0900

    fix(menu): prevent event bubbling?

--- js/index.js
+++ js/index.js
@@ -1,72 +1,29 @@
 const menus = document.querySelectorAll('.menu');
 const subMenus = document.querySelectorAll('.sub_menu');
 
-menus.forEach(menu => {
-    menu.addEventListener('click', (e) => {
-        const index = Array.from(menus).indexOf(e.currentTarget);
-
-        // Deactivate menus and sub menus
-        menus.forEach((item) => {
-            item.classList.remove('activate');
-        })
-        subMenus.forEach((item) => {
-            item.classList.remove('activate');
-        })
-
-        if(menus[index].classList.contains('activate')) {
-            menus[index].classList.remove('activate');
-            subMenus[index].classList.remove('activate');
+menus.forEach((menu, index) => {
+    menu.addEventListener('click', () => {
+        if (menu.classList.contains('active')) {
+            menu.classList.remove('active');
+            subMenus.forEach((subMenus) => {
+                subMenus.classList.remove('active');
+            })
         } else {
-            menus[index].classList.add('activate');
-            subMenus[index].classList.add('activate');
+            menu.classList.add('active');
+            subMenus[index].classList.add('active');
         }
     })
 })
 
+// Prevent event bubbling
+subMenus.forEach(subMenu => {
+    subMenu.addEventListener('click', (e) => {
+        e.stopPropagation();
+        menus.forEach(menu => {
+            menu.classList.remove('active');
+        })
+        subMenu.classList.remove('active');
 
-// let selectedMenu = null;
-// let menuClicked = false;
-//
-// const menu = document.querySelectorAll('.menu');
-// const subMenu = document.querySelectorAll('.menu .sub_menu');
-//
-// menu.forEach((menuItem, index) => {
-//     menuItem.addEventListener('click', () => {
-//         menuClicked = true;
-//         selectedMenu = index;
-//
-//         menu.forEach((item, idx) => {
-//             if(idx !== selectedMenu) {
-//                 item.classList.remove('active');
-//             }
-//         })
-//
-//         subMenu.forEach((item, idx) => {
-//             if(idx !== selectedMenu) {
-//                 item.classList.remove('active');
-//             }
-//         })
-//
-//         menuItem.classList.add('active');
-//         subMenu[selectedMenu].classList.add('active');
-//     })
-// })
-//
-// const aboutUs = document.querySelector('#aboutUs');
-// const isAboutUsOn = false;
-//
-// document.getElementById('aboutUsLink').addEventListener('click', showAboutUs);
-//
-// function showAboutUs() {
-//     if (!isAboutUsOn) {
-//         aboutUs.style.display = 'block';
-//     }
-//
-//     for(let i=0; i<menu.length; i++){
-//         menu[i].classList.remove('active');
-//     }
-//
-//     for(let i=0; i<subMenu.length; i++){
-//         subMenu[i].classList.remove('active');
-//     }
-// }
+    })
+})
+
