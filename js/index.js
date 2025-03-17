9013bcbbcf3c67f0ffaa83f81fef8c1ee8531de3
KangChanil <cikang@eqqui.com>
2025-03-18 04:31:57 +0900

    진행 중 푸시

--- js/index.js
+++ js/index.js
@@ -1,55 +1,72 @@
-let selectedMenu = null;
-let menuClicked = false;
+const menus = document.querySelectorAll('.menu');
+const subMenus = document.querySelectorAll('.sub_menu');
 
-const menu = document.querySelectorAll('.menu');
-const subMenu = document.querySelectorAll('.sub_menu');
+menus.forEach(menu => {
+    menu.addEventListener('click', (e) => {
+        const index = Array.from(menus).indexOf(e.currentTarget);
 
-menu.forEach((menuItem, index) => {
-    menuItem.addEventListener('click', () => {
-        menuClicked = true;
-        selectedMenu = index;
-
-        menu.forEach((item, idx) => {
-            if(idx !== selectedMenu) {
-                item.classList.remove('active');
-            }
+        // Deactivate menus and sub menus
+        menus.forEach((item) => {
+            item.classList.remove('activate');
+        })
+        subMenus.forEach((item) => {
+            item.classList.remove('activate');
         })
 
-        subMenu.forEach((item, idx) => {
-            if(idx !== selectedMenu) {
-                item.classList.remove('active');
-            }
-        })
-
-        menuItem.classList.add('active');
-        subMenu[selectedMenu].classList.add('active');
-
+        if(menus[index].classList.contains('activate')) {
+            menus[index].classList.remove('activate');
+            subMenus[index].classList.remove('activate');
+        } else {
+            menus[index].classList.add('activate');
+            subMenus[index].classList.add('activate');
+        }
     })
 })
 
-const aboutUs = document.querySelector('#aboutUs');
-const isAboutUsOn = false;
 
-document.getElementById('aboutUsLink').addEventListener('click', showAboutUs);
-
-function showAboutUs() {
-    if (!isAboutUsOn) {
-        aboutUs.style.display = 'block';
-    }
-
-    for(let i=0; i<menu.length; i++){
-        console.log("1")
-        menu[i].classList.remove('active');
-        console.log("2")
-    }
-
-    for(let i=0; i<subMenu.length; i++){
-        console.log("3")
-        subMenu[i].classList.remove('active');
-        console.log("4")
-    }
-}
-
-// addEventListener('DOMContentLoaded', () => {
+// let selectedMenu = null;
+// let menuClicked = false;
 //
+// const menu = document.querySelectorAll('.menu');
+// const subMenu = document.querySelectorAll('.menu .sub_menu');
+//
+// menu.forEach((menuItem, index) => {
+//     menuItem.addEventListener('click', () => {
+//         menuClicked = true;
+//         selectedMenu = index;
+//
+//         menu.forEach((item, idx) => {
+//             if(idx !== selectedMenu) {
+//                 item.classList.remove('active');
+//             }
+//         })
+//
+//         subMenu.forEach((item, idx) => {
+//             if(idx !== selectedMenu) {
+//                 item.classList.remove('active');
+//             }
+//         })
+//
+//         menuItem.classList.add('active');
+//         subMenu[selectedMenu].classList.add('active');
+//     })
 // })
+//
+// const aboutUs = document.querySelector('#aboutUs');
+// const isAboutUsOn = false;
+//
+// document.getElementById('aboutUsLink').addEventListener('click', showAboutUs);
+//
+// function showAboutUs() {
+//     if (!isAboutUsOn) {
+//         aboutUs.style.display = 'block';
+//     }
+//
+//     for(let i=0; i<menu.length; i++){
+//         menu[i].classList.remove('active');
+//     }
+//
+//     for(let i=0; i<subMenu.length; i++){
+//         subMenu[i].classList.remove('active');
+//     }
+// }
